36b416dacf0b133d9ef0a6cd8d5b698159e5e8d4
ramzes1x3 <ramzes1x1@gmail.com>
2022-03-21 22:05:31 +0300

    test

--- application/views/main/objects1.php
+++ application/views/main/objects1.php
@@ -1,15 +1,26 @@
-<header class="masthead" style="background-image: url('/public/images/objects.jpg')">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-                <div class="site-heading">
-                    <h1 class="text-shadow">Таганрогский<br>политехнический музей</h1>
-                    <span class="subheading text-shadow">Экспонаты. Радиотехника и телевидение</span>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Политехнический музей ИТА ЮФУ</title>
+        <meta charset="UTF-8">
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+        <meta name = "description" content = "Политехнический музей ИТА ЮФУ">
+        <link rel="stylesheet" href="/public/styles/style.min.css">
+        <link rel="stylesheet" href="/public/styles/objects1.css">
+        <link rel="shortcut icon" href="/public/images//logo_museum_history.png">
+        <link rel="preload" as="font" type="font/woff2"
+          href="/public/fonts/OpenSans-Regular.woff2" crossorigin>
+        <link rel="preload" as="font" type="font/woff2"
+          href="/public/fonts/OpenSans-Bold.woff2" crossorigin>
+
+        <script src="/public/scripts/jquery.js"></script>
+        <script src="/public/scripts/form.js"></script>
+        <script src="/public/scripts/popper.js"></script>
+        <script src="/public/scripts/bootstrap.js"></script>
+    </head>
+    <body class="page__body">
+        <div class="page-wrapper">
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
@@ -33,3 +44,6 @@
         </div>
     </div>
 </div>
+    </body>
+    <script src="app.min.js"></script>
+</html>
